0944d601e5011550524a7db8ebd0648dabd269ef
Vulcan Dev <vulcanization@proton.me>
2024-06-11 16:00:02 +0800

    Align with artifact naming

--- scripts/00_markets_registry.ts
+++ scripts/00_markets_registry.ts
@@ -1,5 +1,5 @@
 // Import JSON Artifact from Contracts
-import PoolAddressesProviderRegistry from "../artifacts/contracts/protocol/configuration/PoolAddressesProviderRegistry.sol/PoolAddressesProviderRegistry.json";
+import PoolRegistryArtifact from "../artifacts/contracts/protocol/configuration/PoolAddressesProviderRegistry.sol/PoolAddressesProviderRegistry.json";
 
 // Import Main Deploy Script Functions
 import createDeployment from "./utils/createDeployment";
@@ -7,7 +7,7 @@ import createDeployment from "./utils/createDeployment";
 // Deploy PoolAddressesProviderRegistry
 export const deployRegistry = createDeployment({
     key: "PoolAddressesProviderRegistry",
-    artifact: PoolAddressesProviderRegistry,
+    artifact: PoolRegistryArtifact,
     argsFn(state, {owner}) {
         if (owner === undefined) {
             throw new Error(`Missing Deployer address in Registry Provider`)
